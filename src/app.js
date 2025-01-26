5d9b7400fe5e55214d11049ec3760012edbf1345
shivamharsh7 <shivamharsh8@gmail.com>
2025-01-26 22:57:22 +0530

    twwet emplemented

--- src/app.js
+++ src/app.js
@@ -21,8 +21,9 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 
-
+import tweetRouter from './routes/tweet.routes.js'
 //routes declreation
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/tweets",tweetRouter)
 export { app }
